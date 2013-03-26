cb77118d932e25e343d7ab004ae42bf936d586b0
Andrea <andreagot16@gmail.com>
2013-03-26 19:08:00 +0100

    script inserted in aggiungiRicordo. When you change the decade, input value changes consequently | added hidden field in aggiungiRicordo2 (in order to pass values hard to find otherwise)

--- reminiscence/main/views.py
+++ reminiscence/main/views.py
@@ -93,6 +93,7 @@ def verificaAdd(request):
 def verificaAdd2(request):
 
 	#SE I DATI NON SONO CORRETTI CHIAMA IL TEMPLATE aggiungiRicordo2Err (gia creato in main/aggiungi/aggiungiRicordo2Err)
+
 	titolo= request.POST.get('titolo')
 	descrizione = request.POST.get('descrizione')
 
