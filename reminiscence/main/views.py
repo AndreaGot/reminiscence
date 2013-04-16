084baba7d051a8845260237106ad91969af4ae08
Andrea Gottardi <andreagot16@gmail.com>
2013-04-16 12:42:04 +0200

    timeline works

--- reminiscence/main/views.py
+++ reminiscence/main/views.py
@@ -193,12 +193,12 @@ def verificaAdd2(request):
 	
 	return render(request,'main/aggiungi/confermaInsert.html')
 
--#
--# GESTIONE TIMELINE
--#
+#
+# GESTIONE TIMELINE
+#
  
--def timeline (request):
--  return render (request, 'main/timeline/timeline.html')
+def timeline (request):
+	return render (request, 'main/timeline/timeline.html')
 
 
 #
